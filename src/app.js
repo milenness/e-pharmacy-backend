4444b1ac54a34e99b7588b3ee7b6b69763ce0dbb
Milena Karpenko <milenness06@gmail.com>
2026-09-24 20:33:10 +0300

    Refactor app.js to organize middleware and route definitions

--- src/app.js
+++ src/app.js
@@ -2,27 +2,35 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const authRouter = require("./routes/api/auth");
+const userRouter = require("./routes/api/user");
+const productsRouter = require("./routes/api/products");
+const storesRouter = require("./routes/api/stores");
+const reviewsRouter = require("./routes/api/reviews");
+const cartRouter = require("./routes/api/cart");
+
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-// Middleware
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json()); // Дозволяє читати JSON з body запитів
+app.use(express.json());
 
-// Тут пізніше будуть наші роути (api/auth, api/products тощо)
-// app.use('/api/user', authRouter);
+app.use("/api/user", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/stores", storesRouter);
+app.use("/api/customer-reviews", reviewsRouter);
+app.use("/api/cart", cartRouter); 
 
-// Обробка неіснуючих маршрутів (404)
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-// Централізована обробка помилок (500)
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
